1e72b73e5b10378922d87944cf09c7f6dc58c0c1
MnlPhlp <MnlPhlp31@gmail.com>
2019-10-28 14:43:47 +0100

    added cli

--- src/nimagehide.nim
+++ src/nimagehide.nim
@@ -1,9 +1,12 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
 
-import nimagehidepkg/[imageIO,storeData,helpers]
+import nimagehidepkg/[imageIO,storeData,helpers],os
 export imageIO,storeData,helpers
 
 when isMainModule:
-  import nimagehidepkg/cli
-  interactiveMenu()
+  import nimagehidepkg/cli,cligen
+  if paramCount() == 0:
+    interactiveMenu()
+  else:
+    dispatchMulti([hide],[discover])
--- src/nimagehidepkg/cli.nim
+++ src/nimagehidepkg/cli.nim
@@ -1,2 +1,12 @@
+import ../nimagehide
+
 proc interactiveMenu*() = 
   echo "Menu"
+
+proc hide*(image: string,secret = "hello World",output: string) =
+  discard
+
+proc discover*(images: seq[string]) =
+  for image in images:
+    let img = loadImage(image)
+    echo img.discoverData().toString()
